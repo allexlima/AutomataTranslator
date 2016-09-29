b7fcafcede9fcfc582bceb3b4a12dd631f2a3256
danielbispov <danielbispov@gmail.com>
2016-09-29 14:08:15 -0400

    Opção no menu para adicionar arquivos; Obteção de caminho do arquivo e leitura

--- qt_interface.py
+++ qt_interface.py
@@ -5,6 +5,7 @@ from PyQt4.QtGui import *
 
 class Interface():
     def __init__(self):
+        # Confiduração da janela principal
         self.app = QApplication(sys.argv)
         self.width = 800
         self.height = 480
@@ -14,33 +15,37 @@ class Interface():
         self.w.setFixedSize(self.width, self.height)
         self.center_widget()
 
+        # Configuração da barra de menu
         self.menu_bar = QMenuBar(self.w)
         self.menu_opcoes = self.menu_bar.addMenu("Opcoes")
         self.sair = QAction("Sair", self.w)
         self.sair.triggered.connect(qApp.quit)
         self.abrir_arquivo = QAction("Abrir", self.w)
-        self.abrir_arquivo.triggered.connect(qApp.quit)
+        self.abrir_arquivo.triggered.connect(self.open_read_file)
         self.menu_opcoes.addAction(self.abrir_arquivo)
         self.menu_opcoes.addAction(self.sair)
 
-
+        # Primeira caixa de texto(input)
         self.txt = QTextEdit(self.w)
         self.txt.setObjectName("text")
         self.txt.setText("Insira JSON aqui")
         self.txt.setGeometry(30, 40, 300, 360)
 
+        # Segunda caixa de texto(output)
         self.out = QTextEdit(self.w)
         self.out.setReadOnly(True)
         self.out.setStyleSheet("background: #EBEBE4;")
         self.out.setObjectName("output")
         self.out.setGeometry(470, 40, 300, 360)
 
+        # Botão para iniciar a conversão do input
         self.convert_button = QPushButton(self.w)
         self.convert_button.setObjectName("convert")
         self.convert_button.setText("Converter")
         self.convert_button.clicked.connect(self.button_convert)
         self.convert_button.move(686, 420)
 
+        # Botão para limpar a caixa de texto "input"
         self.clear_button = QPushButton(self.w)
         self.clear_button.setObjectName("clear")
         self.clear_button.setText("Limpar Input")
@@ -61,5 +66,17 @@ class Interface():
         resolution = QDesktopWidget().screenGeometry()
         self.w.move((resolution.width() / 2) - (self.width / 2),
                   (resolution.height() / 2) - (self.height / 2))
+
+    def open_read_file(self):
+        #fi_le = QFileDialog()
+        file_path = QFileDialog().getOpenFileName(self.w, "Open File")
+        print file_path
+
+        with open(file_path, 'r') as my_file:
+            data = my_file.read()
+
+        print data
+
+
 
 interface = Interface()
