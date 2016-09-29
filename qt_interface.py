b3caba5f2fd93266c9ccbf24929dc4585750b382
danielbispov <danielbispov@gmail.com>
2016-09-29 16:22:52 -0400

    Funcionalidade de salvar AFD gerado

--- qt_interface.py
+++ qt_interface.py
@@ -2,6 +2,7 @@
 import sys
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
+from datetime import datetime
 
 class Interface():
     def __init__(self):
@@ -22,7 +23,11 @@ class Interface():
         self.sair.triggered.connect(qApp.quit)
         self.abrir_arquivo = QAction("Abrir", self.w)
         self.abrir_arquivo.triggered.connect(self.open_read_file)
+        self.salvar_arquivo = QAction("Salvar AFD Gerado", self.w)
+        self.salvar_arquivo.triggered.connect(self.save_afd)
+
         self.menu_opcoes.addAction(self.abrir_arquivo)
+        self.menu_opcoes.addAction(self.salvar_arquivo)
         self.menu_opcoes.addAction(self.sair)
 
         # Primeira caixa de texto(input)
@@ -70,12 +75,17 @@ class Interface():
     def open_read_file(self):
         #fi_le = QFileDialog()
         file_path = QFileDialog().getOpenFileName(self.w, "Open File")
-        print file_path
-
         with open(file_path, 'r') as my_file:
             data = my_file.read()
 
-        print data
+        self.txt.setText(data)
+
+    def save_afd(self):
+        afd_content = self.txt.toPlainText()
+        save_path = QFileDialog().getSaveFileName(self.w, "Save File")
+        print save_path
+        with open(save_path, "w+") as afd:
+            afd.write(afd_content)
 
 
 
