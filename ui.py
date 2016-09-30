01468a53d63a6d974dedd4ced7149e22274ce347
danielbispov <danielbispov@gmail.com>
2016-09-30 16:52:22 -0400

    Botões no menu para importar e exportar JFLAP, mudanças na barra de menu

--- ui.py
+++ ui.py
@@ -12,88 +12,137 @@ class Interface():
         self.height = 480
         self.w = QWidget()
         self.w.setWindowTitle("AutomataTranslator")
-        #self.w.setGeometry(100, 100, 800, 450)
         self.w.setFixedSize(self.width, self.height)
         self.center_widget()
 
         # Configuração da barra de menu
+        self.build_menu()
+
+        # Caixas de texto(input e output)
+        self.build_io()
+
+        # Botões Inferiores
+        self.build_buttons()
+
+        # Execução do Aplicativo e Condição de Saída
+        self.w.show()
+        sys.exit(self.app.exec_())
+
+
+    # Construção da Barra de Menu
+    def build_menu(self):
         self.menu_bar = QMenuBar(self.w)
-        self.menu_opcoes = self.menu_bar.addMenu(u"Opções")
+        self.menu_bar.setFixedWidth(self.width)
+        self.menu_arquivo = self.menu_bar.addMenu(u"Arquivo")
+        self.menu_ajuda = self.menu_bar.addMenu(u"Ajuda")
+
+        self.abrir_arquivo = QAction("Abrir JSON", self.w)
+        self.abrir_arquivo.triggered.connect(self.open_json)
+        self.salvar_arquivo = QAction("Salvar JSON", self.w)
+        self.salvar_arquivo.triggered.connect(self.save_afd)
+        self.exportar = QAction("Exportar JFLAP", self.w)
+        self.exportar.triggered.connect(self.export_jflap)
+        self.importar = QAction("Importar JFLAP", self.w)
+        self.importar.triggered.connect(self.import_jflap)
         self.sair = QAction("Sair", self.w)
         self.sair.triggered.connect(qApp.quit)
-        self.abrir_arquivo = QAction("Abrir", self.w)
-        self.abrir_arquivo.triggered.connect(self.open_read_file)
-        self.salvar_arquivo = QAction("Salvar AFD Gerado", self.w)
-        self.salvar_arquivo.triggered.connect(self.save_afd)
-        #self.menu_bar.setStyleSheet("width: 100%; background: black;")
-        self.menu_bar.setFixedWidth(self.width)
 
-        self.menu_opcoes.addAction(self.abrir_arquivo)
-        self.menu_opcoes.addAction(self.salvar_arquivo)
-        self.menu_opcoes.addAction(self.sair)
+        self.menu_arquivo.addAction(self.abrir_arquivo)
+        self.menu_arquivo.addAction(self.salvar_arquivo)
+        self.menu_arquivo.addSeparator()
+        self.menu_arquivo.addAction(self.importar)
+        self.menu_arquivo.addAction(self.exportar)
+        self.menu_arquivo.addSeparator()
+        self.menu_arquivo.addAction(self.sair)
 
-        # Primeira caixa de texto(input)
-        self.txt = QTextEdit(self.w)
-        self.txt.setObjectName("text")
-        self.txt.setText("Insira JSON aqui")
-        self.txt.setGeometry(30, 40, 350, 360)
+        self.sobre = QAction("Sobre", self.w)
 
-        # Segunda caixa de texto(output)
-        self.out = QTextEdit(self.w)
-        self.out.setReadOnly(True)
-        self.out.setStyleSheet("background: #EBEBE4;")
-        self.out.setObjectName("output")
-        self.out.setGeometry(410, 40, 350, 360)
+        self.menu_ajuda.addAction(self.sobre)
 
-        # Botão para iniciar a conversão do input
+    # Construção das Caixas de Texto
+    def build_io(self):
+        self.input = QTextEdit(self.w)
+        self.input.setObjectName("text")
+        self.input.setText("Insira JSON aqui")
+        self.input.setTabStopWidth(12)
+        self.input.setGeometry(30, 40, 350, 360)
+
+        self.output = QTextEdit(self.w)
+        self.output.setReadOnly(True)
+        self.output.setStyleSheet("background: #EBEBE4;")
+        self.output.setObjectName("output")
+        self.output.setTabStopWidth(12)
+        self.output.setGeometry(410, 40, 350, 360)
+
+    # Construção dos Botões Inferiores
+    def build_buttons(self):
+        # Botão para executar conversão do input
         self.convert_button = QPushButton(self.w)
         self.convert_button.setObjectName("convert")
         self.convert_button.setText("Converter")
-        self.convert_button.clicked.connect(self.button_convert)
+        self.convert_button.clicked.connect(self.convert_input)
         self.convert_button.move(self.width - 115, 425)
 
         # Botão para limpar a caixa de texto "input"
         self.clear_button = QPushButton(self.w)
         self.clear_button.setObjectName("clear")
         self.clear_button.setText("Limpar Input")
-        self.clear_button.clicked.connect(self.button_clear)
+        self.clear_button.clicked.connect(self.clear_input)
         self.clear_button.move(self.width - 220, 425)
 
-        self.w.show()
-        sys.exit(self.app.exec_())
-
-    def button_convert(self):
-        texto = self.txt.toPlainText()
-        self.show_error("his is the description of the error 0xcc98")
+    # Converter Input
+    def convert_input(self):
+        texto = self.input.toPlainText()
         return texto
 
-    def button_clear(self):
-        self.txt.setText("")
+    # Limpar Input
+    def clear_input(self):
+        self.input.setText("")
 
+    # Centralizar Aplicação na Tela do Computador
     def center_widget(self):
         resolution = QDesktopWidget().screenGeometry()
         self.w.move((resolution.width() / 2) - (self.width / 2),
                   (resolution.height() / 2) - (self.height / 2))
 
-    def open_read_file(self):
+    # Abrir Arquivos
+    def open_json(self):
         #fi_le = QFileDialog()
-        file_path = QFileDialog().getOpenFileName(self.w, "Open File", "/home", "JSON (*.json)")
+        file_path = QFileDialog().getOpenFileName(self.w, "Abrir Arquivo", "/home", "JSON (*.json)")
 
         if file_path:
             with open(file_path, 'r') as my_file:
                 data = my_file.read()
-            self.txt.setText(data)
+            self.input.setText(data)
 
+    def import_jflap(self):
+        file_path = QFileDialog().getOpenFileName(self.w, "Importar JFLAP", "/home", "JFLAP (*.jff)")
 
+        if file_path:
+            with open(file_path, 'r') as jflap_file:
+                data = jflap_file.read()
+            return data
+
+    # Salvar Arquivos
     def save_afd(self):
-        afd_content = self.txt.toPlainText()
-        save_path = QFileDialog().getSaveFileName(self.w, "Save File", "/home", "JSON (*.json)")
+        afd_content = self.output.toPlainText()
+        save_path = QFileDialog().getSaveFileName(self.w, "Salvar Arquivo", "/home", "JSON (*.json)")
 
         if save_path:
             with open(save_path + ".json", "w+") as afd:
                 afd.write(afd_content)
-            print save_path
+            return True
 
+    def export_jflap(self):
+        save_path = QFileDialog().getSaveFileName(self.w, "Exportar JFLAP", "/home", "JFLAP (*.jff)")
+        afd_content = self.output.toPlainText()
+
+        if save_path:
+            with open(save_path + ".jff", 'w+') as jflap_file:
+                jflap_file.write(afd_content)
+            return True
+
+    # Manipulador de Erros
     def show_error(self, msg):
         message = QMessageBox(self.w)
         message.setIcon(2)
@@ -102,6 +151,7 @@ class Interface():
         message.setWindowModality(Qt.ApplicationModal)
 
         message.exec_()
+
 
 
 interface = Interface()
