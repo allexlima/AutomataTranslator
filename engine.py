71a3d92d13bdd0a9ac8c0da36e58377ad7c08ca0
Allex Lima <allexlima97@gmail.com>
2016-09-30 12:25:56 -0400

    Adicionando leitura e verificação da estrutura de I/O

--- engine.py
+++ engine.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-if __name__ == "__main__":
-    pass
+import json
+import jsonschema
+
+
+class Engine:
+    def __init__(self, input):
+        self.model_file = "model/struct.json"
+        self.__afn = input
+
+    def read_schema(self):
+        with open(self.model_file) as data_file:
+            return json.load(data_file)
+
+    def check_syntax(self):
+        try:
+            jsonschema.Draft4Validator(self.read_schema()).validate(self.__afn)
+            return True
+        except jsonschema.exceptions.ValidationError as error:
+            return error.message.replace("u'", "'")
