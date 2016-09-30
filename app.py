e254421709fa2e22c91186339bf7ad83e53c12ba
Allex Lima <allexlima97@gmail.com>
2016-09-30 14:16:02 -0400

    Centralizando funções na app.py

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from ui import Interface
+import json
+from engine import Engine
+# from ui import Interface
 
 if __name__ == "__main__":
-    Interface()
+
+    with open("models/json/example.json", "r") as fl:
+        example = json.load(fl)
+
+    test = Engine(example)
+    if test.check_syntax() is not True:
+        print test.check_syntax()
--- engine.py
+++ engine.py
@@ -8,15 +8,31 @@ import jsonschema
 class Engine:
     def __init__(self, input):
         self.model_file = "models/json/struct.json"
+        self.__message = None
         self.__afn = input
 
+    def alerts(self):
+        return self.__message
+
     def read_schema(self):
-        with open(self.model_file) as data_file:
-            return json.load(data_file)
+        try:
+            with open(self.model_file, "r") as data_file:
+                return json.load(data_file)
+        except IOError as error:
+            self.__message = "Erro ler modelo de I/O!"
+            return None
 
     def check_syntax(self):
         try:
             jsonschema.Draft4Validator(self.read_schema()).validate(self.__afn)
             return True
         except jsonschema.exceptions.ValidationError as error:
-            return error.message.replace("u'", "'")
+            self.message = error.message.replace("u'", "'")
+        except AttributeError:
+            self.__message = "Erro ao definir estrutura dos autômatos para validação!"
+        return False
+
+    def main(self):
+        pass
+
+
